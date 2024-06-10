e0d7b39d299eb225bedbb0bb4c61146bef7e284b
Laqub Haroon <145701125+Laqub-Haroon@users.noreply.github.com>
2024-06-11 02:20:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text+= page.extract_text()
     return  text
@@ -94,7 +94,6 @@ def main():
     # Center the file uploader and button
     col1, col2, col3 = st.columns([1, 4, 1])
     with col2:
-        
         pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True, type="pdf")
         if st.button("Submit & Process"):
             if pdf_docs is not None:
